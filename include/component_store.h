837c62637699eb0ad41e51caa01f106bc4846b56
Yunze Liao <yunze2@illinois.edu>
2026-05-19 14:50:13 -0500

    ecs core

--- include/component_store.h
+++ include/component_store.h
@@ -6,25 +6,29 @@
 namespace tobe {
 class IComponentStore {
 public:
+  virtual bool remove(Entity e) = 0;
   virtual ~IComponentStore() = default;
 };
 
 template <typename T> class ComponentStore : public IComponentStore {
+private:
   std::unordered_map<Entity, T> data;
 
 public:
-  void add(Entity e, T component) { data.emplace(e, std::move(component)); }
+  void add(Entity e, T component) { data.insert_or_assign(e, std::move(component)); }
+
   T *get(Entity e) {
     auto it = data.find(e);
     return it == data.end() ? nullptr : &(it->second);
   }
-  // const overload
+
   const T *get(Entity e) const {
     auto it = data.find(e);
     return it == data.end() ? nullptr : &(it->second);
   }
+
   bool has(Entity e) const { return data.contains(e); }
 
-  bool remove(Entity e) { return data.erase(e); }
+  bool remove(Entity e) override { return data.erase(e); }
 };
 } // namespace tobe
---
+++ include/world.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "component_store.h"
+#include "entity.h"
+#include <memory>
+#include <typeindex>
+#include <unordered_map>
+#include <unordered_set>
+
+namespace tobe {
+class World {
+private:
+  Entity next_entity = 0;
+  std::unordered_set<Entity> alive;
+  std::unordered_map<std::type_index, std::unique_ptr<IComponentStore>> stores;
+
+  template <typename T> ComponentStore<T> &get_store() {
+    auto idx = std::type_index(typeid(T));
+    auto [it, _] =
+        stores.try_emplace(idx, std::make_unique<ComponentStore<T>>());
+    return *static_cast<ComponentStore<T> *>(it->second.get());
+  }
+
+  template <typename T> const ComponentStore<T> *get_store() const {
+    auto idx = std::type_index(typeid(T));
+    auto it = stores.find(idx);
+    if (it == stores.end())
+      return nullptr;
+    return static_cast<const ComponentStore<T> *>(it->second.get());
+  }
+
+public:
+  Entity create_entity() {
+    alive.insert(++next_entity);
+    return next_entity;
+  }
+  void destroy_entity(Entity entity) {
+    alive.erase(entity);
+    for (auto &[type, store] : stores) {
+      store->remove(entity);
+    }
+  }
+
+  template <typename T> void add_component(Entity entity, T component) {
+    get_store<T>().add(entity, std::move(component));
+  }
+
+  template <typename T> T *get_component(Entity entity) {
+    return get_store<T>().get(entity);
+  }
+
+  template <typename T> const T *get_component(Entity entity) const {
+    auto *store = get_store<T>();
+    return store ? store->get(entity) : nullptr;
+  }
+
+  template <typename T> bool has_component(Entity entity) const {
+    auto *store = get_store<T>();
+    return store && store->has(entity);
+  }
+
+  template <typename T> bool remove_component(Entity entity) {
+    return get_store<T>().remove(entity);
+  }
+};
+} // namespace tobe
